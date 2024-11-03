f0e6fda3add32b322c31132b2073b949519c85d7
Leroyalle <nikolajmelonov@yandex.ru>
2024-11-03 17:58:41 +0300

    comment service

---
+++ src/app/services/comment-api.ts
@@ -0,0 +1,24 @@
+import { api } from "./api"
+
+export const commentApi = api.injectEndpoints({
+  endpoints: builder => ({
+    createComment: builder.mutation<Comment, Partial<Comment>>({
+      query: commentData => ({
+        url: "/comments",
+        method: "POST",
+        body: commentData,
+      }),
+    }),
+    deleteComment: builder.mutation<void, string>({
+      query: id => ({
+        url: `/comment/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+})
+
+export const { useCreateCommentMutation, useDeleteCommentMutation } = commentApi
+export const {
+  endpoints: { createComment, deleteComment },
+} = commentApi
