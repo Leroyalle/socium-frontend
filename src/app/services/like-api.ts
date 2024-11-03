c92d2da29b00a3ff8821d7c50c5ca94af548381e
Leroyalle <nikolajmelonov@yandex.ru>
2024-11-03 18:34:43 +0300

    like service

---
+++ src/app/services/like-api.ts
@@ -0,0 +1,25 @@
+import type { Like } from "../types"
+import { api } from "./api"
+
+export const likeApi = api.injectEndpoints({
+  endpoints: builder => ({
+    likePost: builder.mutation<Like, { postId: string }>({
+      query: id => ({
+        url: "/likes",
+        method: "POST",
+        body: id,
+      }),
+    }),
+    unlikePost: builder.mutation<void, string>({
+      query: id => ({
+        url: `/likes/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+})
+
+export const { useLikePostMutation, useUnlikePostMutation } = likeApi
+export const {
+  endpoints: { likePost, unlikePost },
+} = likeApi
