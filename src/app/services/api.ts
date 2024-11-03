5533d5b93b98eacaa32ab5303af997bd9cf45bf2
Leroyalle <nikolajmelonov@yandex.ru>
2024-11-03 17:38:14 +0300

    userApi reducer

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,17 +1,17 @@
-import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query"
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react"
 import { BASE_URL } from "../../constans"
 import type { RootState } from "../store"
 
 const baseQuery = fetchBaseQuery({
   baseUrl: `${BASE_URL}/api`,
-  prepareHeaders: (headers, { getState }) => {
-    const token =
-      (getState() as RootState).auth.token || localStorage.getItem("token")
-    if (token) {
-      headers.set("Authorization", `Bearer ${token}`)
-    }
-    return headers
-  },
+  // prepareHeaders: (headers, { getState }) => {
+  //   const token =
+  //     (getState() as RootState).auth.token || localStorage.getItem("token")
+  //   if (token) {
+  //     headers.set("Authorization", `Bearer ${token}`)
+  //   }
+  //   return headers
+  // },
 })
 
 const baseQueryWithRetry = retry(baseQuery, {
